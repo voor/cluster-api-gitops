eb8aa577d36496b3306e725517ddf595f80ed18c
Robert Van Voorhees <rvanvoorhees@vmware.com>
2020-05-03 09:20:41 -0400

    Remove status from CRDs.

--- workload/secrets/seal.sh
+++ workload/secrets/seal.sh
@@ -5,8 +5,8 @@ secret_files="${1:-secrets/*.yaml}"
 
 for secret in $secret_files
 do
-    nopath=${secret:8}
-    sealed=${nopath%.yaml}
-    output=${sealed%-config}
+    nopath=${secret:8} # Strips off secrets/ folder name
+    sealed=${nopath%.yaml} # Removes yaml at the end
+    output=${sealed%-config} # Remove config at the end.
     kubectl create -f ${secret} --dry-run=client -o json | kubeseal --cert workload-secrets.pem -o yaml > ${output}/manifests/${sealed}-sealed.yaml
 done
