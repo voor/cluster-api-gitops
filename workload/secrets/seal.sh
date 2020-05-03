fef00fa80799f303ee7397c5180d917fbe9f460f
Robert Van Voorhees <rvanvoorhees@vmware.com>
2020-05-03 10:49:02 -0400

    Add in a reverse proxy for sonarqube.

--- workload/secrets/seal.sh
+++ workload/secrets/seal.sh
@@ -7,6 +7,7 @@ for secret in $secret_files
 do
     nopath=${secret:8} # Strips off secrets/ folder name
     sealed=${nopath%.yaml} # Removes yaml at the end
-    output=${sealed%-config} # Remove config at the end.
-    kubectl create -f ${secret} --dry-run=client -o json | kubeseal --cert workload-secrets.pem -o yaml > ${output}/manifests/${sealed}-sealed.yaml
+    outputfolder=${sealed%.*} # Removes optional numbering
+    output=${outputfolder%-config} # Remove config at the end.
+    kubectl create -f ${secret} --dry-run=client -o json | kubeseal --cert workload-secrets.pem -o yaml > ${output}/manifests/${sealed}.sealed.yaml
 done
