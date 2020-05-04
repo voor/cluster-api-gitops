8b4f2eb6d959c7ee630cccda124dbe0242d0a50c
Robert Van Voorhees <rvanvoorhees@vmware.com>
2020-05-04 09:47:11 -0400

    Tweak how manifests are generated.

--- workload/dex/helm/generate-manifests.sh
+++ workload/dex/helm/generate-manifests.sh
@@ -4,4 +4,4 @@ set -eux
 helm repo add stable https://kubernetes-charts.storage.googleapis.com/
 helm repo update
 
-helm template dex stable/dex -f dex/helm/dex-values.yaml | ytt --ignore-unknown-comments -f- -f dex/helm/overlay-helmtemplate.yaml > dex/manifests/dex.yaml
+helm template dex stable/dex -f dex/helm/dex-values.yaml --namespace sso | ytt --ignore-unknown-comments -f- -f dex/helm/overlay-helmtemplate.yaml > dex/manifests/dex.yaml
--- workload/sonarqube/helm/generate-manifests.sh
+++ workload/sonarqube/helm/generate-manifests.sh
@@ -6,6 +6,6 @@ helm repo add oteemocharts https://oteemo.github.io/charts
 helm repo add stable https://kubernetes-charts.storage.googleapis.com/
 helm repo update
 
-helm template sonarqube-proxy stable/oauth2-proxy -f sonarqube/helm/oauth2-proxy-values.yaml | ytt --ignore-unknown-comments -f- -f sonarqube/helm/overlay-helmtemplate.yaml > sonarqube/manifests/sonarqube-proxy.yaml
-helm template sonarqube-db bitnami/postgresql -f sonarqube/helm/postgresql-values.yaml | ytt --ignore-unknown-comments -f- -f sonarqube/helm/overlay-helmtemplate.yaml > sonarqube/manifests/sonarqube-db.yaml
-helm template sonarqube oteemocharts/sonarqube -f sonarqube/helm/sonarqube-values.yaml | ytt --ignore-unknown-comments -f- -f sonarqube/helm/overlay-helmtemplate.yaml > sonarqube/manifests/sonarqube.yaml
+helm template sonarqube-proxy stable/oauth2-proxy -f sonarqube/helm/oauth2-proxy-values.yaml --namespace sonar | ytt --ignore-unknown-comments -f- -f sonarqube/helm/overlay-helmtemplate.yaml > sonarqube/manifests/sonarqube-proxy.yaml
+helm template sonarqube-db bitnami/postgresql -f sonarqube/helm/postgresql-values.yaml --namespace sonar | ytt --ignore-unknown-comments -f- -f sonarqube/helm/overlay-helmtemplate.yaml > sonarqube/manifests/sonarqube-db.yaml
+helm template sonarqube oteemocharts/sonarqube -f sonarqube/helm/sonarqube-values.yaml --namespace sonar | ytt --ignore-unknown-comments -f- -f sonarqube/helm/overlay-helmtemplate.yaml > sonarqube/manifests/sonarqube.yaml
