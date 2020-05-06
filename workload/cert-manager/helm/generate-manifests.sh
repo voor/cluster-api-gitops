f05900812d5c2935b249fb5c91f1e0ccdf4f770d
Robert Van Voorhees <rvanvoorhees@vmware.com>
2020-05-05 20:54:07 -0400

    cert-manager as helm chart.

---
+++ workload/cert-manager/helm/generate-manifests.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+set -eux
+
+helm repo add jetstack https://charts.jetstack.io
+helm repo update
+
+helm template cert-manager jetstack/cert-manager -f cert-manager/helm/cert-manager-values.yaml --namespace cert-manager > cert-manager/manifests/cert-manager.yaml
