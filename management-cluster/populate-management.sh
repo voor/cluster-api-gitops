a8fe8162f6e16915d3812d625c2c3c387a406ddb
Robert Van Voorhees <rvanvoorhees@vmware.com>
2020-05-09 19:44:35 -0400

    Have a single yaml that gets applied.

--- management-cluster/populate-management.sh
+++ management-cluster/populate-management.sh
@@ -5,5 +5,5 @@ set -eux
 echo "Populates the management cluster with necessary apps."
 
 for app in sealed-secrets kapp-controller workload-clusters; do
-    ytt --ignore-unknown-comments -f deploy/${app} -f deploy/values.yaml | kapp -y deploy -a ${app} -f -
+    ytt --ignore-unknown-comments -f deploy/${app} -f deploy/values.yaml | kapp -y deploy -n kube-system -a ${app} -f -
 done
