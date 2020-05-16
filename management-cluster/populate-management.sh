374fa56252aae8005cc4e0f300465a64ff72ccea
Robert Van Voorhees <rvanvoorhees@vmware.com>
2020-05-16 10:19:45 -0400

    Use existing master key if present.

--- management-cluster/populate-management.sh
+++ management-cluster/populate-management.sh
@@ -4,6 +4,11 @@ set -eux
 
 echo "Populates the management cluster with necessary apps."
 
+# If we have an existing secret apply it first.
+if [ -f "master.key" ]; then
+    kubectl apply -f master.key
+fi
+
 for app in sealed-secrets kapp-controller workload-clusters; do
     ytt --ignore-unknown-comments -f deploy/${app} | kapp -y deploy -n kube-system -a ${app} -f -
 done
