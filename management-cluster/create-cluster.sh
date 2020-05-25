0db45f89d3c29c207da26d50840fd9f56f13ab9b
Robert Van Voorhees <rvanvoorhees@vmware.com>
2020-05-24 20:24:02 -0400

    Change how clusters are created now.

--- management-cluster/create-cluster.sh
+++ management-cluster/create-cluster.sh
@@ -17,8 +17,8 @@ CLUSTER_NAME=$1
 WORKING=$(mktemp -d -t ${CLUSTER_NAME}XXXXX)
 shift
 
-tkg create cluster $CLUSTER_NAME $@ --dry-run | ytt --ignore-unknown-comments -f template-assist -f- > workload/manifests/${CLUSTER_NAME}.yaml
-tkg create cluster $CLUSTER_NAME $@ --dry-run | ytt --ignore-unknown-comments -f postcreate-assist -f- > ${WORKING}/${CLUSTER_NAME}.yaml
-kubectl create -f ${WORKING}/${CLUSTER_NAME}.yaml --dry-run=client -o json | kubeseal --cert management-secrets.pem -o yaml > workload/manifests/${CLUSTER_NAME}-postcreate.sealed.yaml \
-  || rm -rf $WORKING
-rm -rf $WORKING
+tkg create cluster $CLUSTER_NAME $@ --dry-run | ytt --ignore-unknown-comments --data-value cluster_name=${CLUSTER_NAME} -f template-assist -f- > workload/manifests/${CLUSTER_NAME}.yaml
+# tkg create cluster $CLUSTER_NAME $@ --dry-run | ytt --ignore-unknown-comments -f postcreate-assist -f- > ${WORKING}/${CLUSTER_NAME}.yaml
+# kubectl create -f ${WORKING}/${CLUSTER_NAME}.yaml --dry-run=client -o json | kubeseal --cert management-secrets.pem -o yaml > workload/manifests/${CLUSTER_NAME}-postcreate.sealed.yaml \
+#   || rm -rf $WORKING
+# rm -rf $WORKING
