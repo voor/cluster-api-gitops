b8b9ee8722ca0c984165f4ac92e46ec143def253
Robert Van Voorhees <rcvanvo@gmail.com>
2020-05-26 16:01:48 -0400

    Change the order of operations.

--- management-cluster/populate-management.sh
+++ management-cluster/populate-management.sh
@@ -9,6 +9,6 @@ if [ -f "master.key" ]; then
     kubectl apply -f master.key
 fi
 
-for app in sealed-secrets workload-secrets kapp-controller workload-clusters; do
+for app in sealed-secrets kapp-controller workload-clusters workload-secrets; do
     ytt --ignore-unknown-comments -f deploy/${app} | kapp -y deploy -n kube-system -a ${app} -f -
 done
