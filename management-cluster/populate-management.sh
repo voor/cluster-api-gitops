8737b2b016c6d785556f18ed1400e0a77a604df7
Robert Van Voorhees <rcvanvo@gmail.com>
2020-06-08 17:33:49 -0400

    Overhaul the management cluster services.

--- management-cluster/populate-management.sh
+++ management-cluster/populate-management.sh
@@ -9,6 +9,6 @@ if [ -f "master.key" ]; then
     kubectl apply -f master.key
 fi
 
-for app in sealed-secrets kapp-controller workload-clusters workload-secrets; do
-    ytt --ignore-unknown-comments -f deploy/${app} | kapp -y deploy -n kube-system -a ${app} -f -
+for app in sealed-secrets kapp-controller workload-clusters workload-secrets gangway; do
+    ytt --ignore-unknown-comments -f deploy/${app}/manifests | kapp -y deploy -n kube-system -a ${app} -f -
 done
