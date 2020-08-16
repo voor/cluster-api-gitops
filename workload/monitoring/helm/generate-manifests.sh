4d105b0ce0557921c2cd0c04c27448211e8bd42f
Robert Van Voorhees <rcvanvo@gmail.com>
2020-08-16 19:50:38 -0400

    Update to include crds.

--- workload/monitoring/helm/generate-manifests.sh
+++ workload/monitoring/helm/generate-manifests.sh
@@ -4,5 +4,5 @@ set -eux
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
 
-helm template monitoring bitnami/prometheus-operator -f monitoring/helm/monitoring-values.yaml | ytt --ignore-unknown-comments -f- -f monitoring/helm/overlay-helmtemplate.yaml > monitoring/manifests/prometheus-operator.yaml
-helm template grafana bitnami/grafana -f monitoring/helm/grafana-values.yaml | ytt --ignore-unknown-comments -f- -f monitoring/helm/grafana-overlay-helmtemplate.yaml > monitoring/manifests/grafana.yaml
+helm template monitoring bitnami/prometheus-operator -f monitoring/helm/monitoring-values.yaml --include-crds | ytt --ignore-unknown-comments -f- -f monitoring/helm/overlay-helmtemplate.yaml > monitoring/manifests/prometheus-operator.yaml
+helm template grafana bitnami/grafana -f monitoring/helm/grafana-values.yaml --include-crds | ytt --ignore-unknown-comments -f- -f monitoring/helm/grafana-overlay-helmtemplate.yaml > monitoring/manifests/grafana.yaml
