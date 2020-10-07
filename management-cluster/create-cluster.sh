40b9534571872ab1046e3ad21d636062195564f6
Robert Van Voorhees <rcvanvo@gmail.com>
2020-10-07 12:38:05 -0400

    Adding watersafety.

--- management-cluster/create-cluster.sh
+++ management-cluster/create-cluster.sh
@@ -17,9 +17,9 @@ export AWS_PRIVATE_NODE_CIDR=unused
 export AWS_PUBLIC_NODE_CIDR=unused
 export AWS_REGION=us-gov-west-1
 export AWS_SSH_KEY_NAME=overridden
-unset AWS_AMI_ID
+export AWS_AMI_ID=ignored
 export AWS_VPC_ID=overriden
 export CONTROL_PLANE_MACHINE_TYPE=m5n.large
 export NODE_MACHINE_TYPE=m5n.large
 
-tkg create cluster -i aws $CLUSTER_NAME $@ --dry-run 2>/dev/null | ytt --ignore-unknown-comments --data-value cluster_name=${CLUSTER_NAME} -f template-assist -f- > workload/manifests/clusters/${CLUSTER_NAME}.yaml
+tkg create cluster -i aws:v0.5.4 $CLUSTER_NAME $@ --dry-run 2>/dev/null | ytt --ignore-unknown-comments --data-value cluster_name=${CLUSTER_NAME} -f template-assist -f- > workload/manifests/clusters/${CLUSTER_NAME}.yaml
