e294b388d154d9cf19a1f6b540ab2f7b6fa67dad
Robert Van Voorhees <rcvanvo@gmail.com>
2020-11-12 10:43:20 -0500

    Downgrade cert-manager.

--- workload/cert-manager/helm/generate-manifests.sh
+++ workload/cert-manager/helm/generate-manifests.sh
@@ -4,4 +4,4 @@ set -eux
 helm repo add jetstack https://charts.jetstack.io
 helm repo update
 
-helm template cert-manager jetstack/cert-manager -f cert-manager/helm/cert-manager-values.yaml --namespace cert-manager --include-crds > cert-manager/manifests/cert-manager.yaml
+helm template cert-manager jetstack/cert-manager --version v0.16.1 -f cert-manager/helm/cert-manager-values.yaml --namespace cert-manager --include-crds > cert-manager/manifests/cert-manager.yaml
